0c2fe57bfd416e1c9b908efe013af2abfcfd1cc9
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-10-06 17:32:29 -0300

    error treatment in order functiuons added

--- src/routes/order.js
+++ src/routes/order.js
@@ -1,16 +1,53 @@
 const router = require('express').Router();
 
 const { handlerOrders } = require('../services/db');
+const { orderErrorMessages } = require('../utils/error_messages');
 
 router.get('/', async (req, res) => {
   try {
-    const id = parseInt(req.query.id);
+    const orders = await handlerOrders.get();
 
+    res.status(200).json(orders);
+
+  } catch (err) {
+    res.status(500).json({"error": err.message});
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    
+    if (id <= 0) {
+      throw new RangeError(orderErrorMessages.invalidIdValue);
+    }
+    if (!id) {
+      throw new TypeError(orderErrorMessages.invalidIdType);
+    }
+    
     const orders = await handlerOrders.get(id);
+
+    if (orders.length === 0) {
+      throw new Error(orderErrorMessages.notfound);
+    }
+
     res.status(200).json(orders);
     
   } catch (err) {
-    console.log(err);
+    switch (err.message) {
+      case orderErrorMessages.notfound:
+        res.status(404).json({"error": err.message});
+      break;
+      case orderErrorMessages.invalidIdType:
+      case orderErrorMessages.invalidIdValue:
+        res.status(400).json({"error": err.message});
+      break;
+    
+      default:
+        res.status(500).json({"error": err.message});
+
+      break;
+    }
   }
 
 })
@@ -21,7 +58,12 @@ router.post('/', async (req, res) => {
     res.status(201).json(newOrder);
     
   } catch (err) {
-    console.log(err);
+
+    if (/REFERENCED/.test(err.code)) {
+      res.status(400).json({"error": orderErrorMessages.invalidClientIdOrProductId});
+    } else {
+      res.status(500).json({"error": err});
+    }
   }
 
 })
---
+++ src/utils/error_messages.js
@@ -0,0 +1,10 @@
+const orderErrorMessages = {
+    "notfound" : "There isn't the specificated order!",
+    "invalidIdType": "The id should be a number!",
+    "invalidIdValue": "The id should be a number greather than 0!",
+    "invalidClientIdOrProductId": "The specificated client Id or product Id there isn't or it's invalid!"
+}
+
+module.exports = {
+    orderErrorMessages
+}
