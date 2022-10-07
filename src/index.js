a5e3c3b7a628fa3fa9437e8f23298911a2642197
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2022-10-07 20:19:46 -0300

    refactoring error handle functions

--- src/index.js
+++ src/index.js
@@ -7,32 +7,13 @@ const port = process.env.PORT;
 const user = require('./routes/user');
 const product = require('./routes/product');
 const order = require('./routes/order');
-const { clientErrorMessages, orderErrorMessages, productErrorMessages, generalErrorMessages } = require('./utils/error_messages');
+const errorTreatment = require('./utils/error_treatment');
 
 app.use(express.json());
 app.use('/user', user);
 app.use('/product', product);
 app.use('/order', order);
-app.use((error, req, res, next) => {
-    switch (error.message) {
-        case clientErrorMessages.notfound:
-        case productErrorMessages.notfound:
-        case orderErrorMessages.notfound:
-            res.status(404).json({"error": error.message});
-        break;
-
-        case generalErrorMessages.invalidIdType:
-        case generalErrorMessages.invalidIdValue:
-        case clientErrorMessages.cpfAlreadyExists:
-        case orderErrorMessages.invalidClientIdOrProductId:
-            res.status(400).json({"error": error.message});
-        break;
-      
-        default:
-            res.status(500).json({"error": error.message});
-        break;
-      }
-})
+app.use((error, req, res, next) => { errorTreatment(error.message, res) })
 
 app.listen(port | 3000, () => {
     console.log(`Rodando o server em http://localhost:${port ? port : 3000}`)
